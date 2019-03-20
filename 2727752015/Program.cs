222f0b58956d87963577ae13218236d531a383c7
Billy Mejia <48001584+BiEdMe@users.noreply.github.com>
2019-03-19 19:56:44 -0600

    Add files via upload

---
+++ 2727752015/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parcial02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+        public int n1, n2;
+
+
+        public void ()
+
+              
+
+    
+         
+
+        }
+    }
+}
